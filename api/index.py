62a3ac10e5dfac9610891f9a8653e2bb583f420e
abrilohd <abrsh067@gmail.com>
2026-09-09 16:37:53 +0300

    fixed the versel issue

--- api/index.py
+++ api/index.py
@@ -11,6 +11,6 @@ os.environ.setdefault("VERCEL", "1")
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
 
-application = get_wsgi_application()
 call_command("collectstatic", interactive=False, verbosity=0)
+application = get_wsgi_application()
 app = application
